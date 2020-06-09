e29b8399bab9c22c0d20ff07246014fd975446ba
Alexander Scott <xander@axrs.io>
2020-06-09 19:29:13 +1000

    Minor refactor

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject io.axrs/prospect "0.0.0-SNAPSHOT"
+  :description "Simple Clojure Namespace function discovery tool for CLIs"
+  :min-lein-version "2.8.1"
+  :dependencies []
+  :clean-targets ["target"]
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}})
---
+++ src/io/axrs/prospect/core.clj
@@ -0,0 +1,51 @@
+(ns io.axrs.prospect.core
+  (:require [clojure.string :as str])
+  (:import
+    (clojure.lang Namespace)))
+
+(defn print-synopsis [fns]
+  (println "SYNOPSIS")
+  (doseq [{:keys [name arglists] :as fn} fns]
+    (print \tab name)
+    (when (seq (first arglists))
+      (print \space (str/join \space arglists)))
+    (print \newline)))
+
+(defn print-description [fns]
+  (println "DESCRIPTION")
+  (doseq [{:keys [name arglists] :as fn} fns]
+    (print \tab name)
+    (when (seq (first arglists))
+      (print \space (str/join \space arglists)))
+    (print \newline)
+    (println \tab \tab (:doc fn))
+    (doseq [arg (distinct (flatten arglists))]
+      (when-let [doc (:doc (meta arg))]
+        (print \tab \tab \tab arg "-" doc \newline)))
+    (print \newline)))
+
+(defn publics [ns]
+  (->> (ns-interns ns)
+       vals
+       (filter #(and (bound? %) (fn? @%)))
+       (map meta)
+       (filter (comp not true? :private))
+       (sort-by :name)))
+
+(defmulti print-help (partial instance? Namespace))
+
+(defmethod print-help true [ns]
+  (print-help (publics ns)))
+
+(defmethod print-help false [publics]
+  (print-synopsis publics)
+  (println)
+  (print-description publics))
+
+(defn run [ns & [f-name & args]]
+  (let [f-sym (some-> f-name symbol)
+        publics (publics ns)]
+    (if (and f-sym (->> publics
+                        (filter (comp (partial = f-sym) :name))
+                        first))
+      (apply (resolve f-sym) args))))
