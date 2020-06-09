24eb73c7fb5ac24f35110d5d2e8e23d9b40fe724
Alexander Scott <xander@axrs.io>
2020-06-09 20:10:11 +1000

    License added

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject io.axrs/prospect "0.0.0-SNAPSHOT"
   :description "Simple Clojure Namespace function discovery tool for CLIs"
+  :url "https://github.com/axrs/prospect"
+  :license {:name         "Eclipse Public License - v 2.0"
+            :url          "https://www.eclipse.org/legal/epl-v20.html"
+            :distribution :repo}
   :min-lein-version "2.8.1"
   :dependencies []
   :clean-targets ["target"]
